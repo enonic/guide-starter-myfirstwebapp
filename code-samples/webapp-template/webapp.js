c93fc8085f4ebb64537aa6c4ae2bf9f37d243382
Christian Westgaard <cwe@enonic.com>
2020-08-06 12:58:58 +0200

    Show example using a "constant" outside get function

--- code-samples/webapp-template/webapp.js
+++ code-samples/webapp-template/webapp.js
@@ -1,14 +1,14 @@
 var thymeleaf = require('/lib/thymeleaf'); // Load template engine
 
+var VIEW = resolve('hello.html') // Lookup template file
+
 exports.get = function (req) {
-  var view = resolve('hello.html') // Lookup template file
   var model = { // Build model object
     title: 'Hello Web app',
     message: 'Views are working too!'
   };
 
-  return  {
-    body: thymeleaf.render(view, model) // Render page
-  }
-
+  return {
+    body: thymeleaf.render(VIEW, model) // Render page
+  };
 };
